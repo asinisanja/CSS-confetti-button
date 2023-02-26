26c741249e7ff6d5f7b04379d4907c85cd6e4ab2
asinisanja <70744933+asinisanja@users.noreply.github.com>
2023-02-26 11:14:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+import React, {useCallback} from "https://cdn.skypack.dev/react@17";
+import {render} from "https://cdn.skypack.dev/react-dom@17";
+import confetti from "https://cdn.skypack.dev/canvas-confetti@1";
+function App(){
+    const onClick = useCallback(()➡️{
+        confetti({
+            particleCount: 150,
+            spread: 60
+        });
+    }, []);
+    return(
+        <button className="button" onClick={onClick}>
+            <span>🎉</span>
+            <span>Like</span>
+        </button>
+    );
+}
+render(<App/>, document, getElementById("root"));
